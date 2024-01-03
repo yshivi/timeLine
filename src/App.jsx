0feb815fc279c46263b16e2c9b0872d58be79db1
shivi yadav <yshivi825@gmail.com>
2024-01-03 16:35:54 +0530

    I made a TimeLine Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Timeline from './Timeline'
+const App = () => {
+  return (
+    <div>
+      <h1> My timeLine</h1>
+      <Timeline year ={2011} event = 'When I was 8 years old. Then I fell from the roof and after that I had to undergo many treatments.'/>
+      <Timeline year ={2022} event = 'In 2022, my mother and brother met with an accident. Then I would have felt very bad. Which caused a lot of trouble. like in studies'/>
+      <Timeline year ={2024} event = 'For Freshers And Experienced. Search Online Work Jobs From Home. Find Your Dream Job. Fresher Jobs. Part Time Jobs. Latest Jobs In 2022. Work From Home Job Option. Get Job Alert. Free Job Alerts. Amenities: Job Search Ads Platform, Full Time & Part Time, Work From Home Options, Apply To Get Hired Fast, Latest Jobs in 2022, Remote Location Jobs.'/>
+      <Timeline/>
+    </div>
+  )
+}
+export default App
+
+
---
+++ src/Timeline.jsx
@@ -0,0 +1,10 @@
+const Timeline = (props)=>{
+    return(
+        <div>
+            <h3>{props.year}</h3>
+            <p>{props.event}</p>
+        </div>
+    ) 
+}
+export default Timeline
+
